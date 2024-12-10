19ee04108be3e405846894d24e5656fef45df53f
malli24 <malleswari82006@gmail.com>
2024-12-10 13:33:21 +0530

    Add files via upload

---
+++ Str.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Str
+{
+    public static void main (String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        String ans=" ";
+        for(int i=0;i<a.length();i+=2)
+        {
+            ans+=a.charAt(i);
+            System.out.println(ans);
+    }   
+    }
+}
