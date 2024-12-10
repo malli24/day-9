98cb8c3f3b4f5e4f2f0c3095fa686186df0b0c04
malli24 <malleswari82006@gmail.com>
2024-12-10 15:43:01 +0530

    Add files via upload

---
+++ foreachstr.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class foreachstr {
+    public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+        String a[] ={"apple","orange","mango"};
+        for(String b:a)
+        System.out.println(b+" ");
+     }
+}
